c02d7c47cc2cee9b0a6fc1603963a52337e94f0d
lexich <lexich121@gmail.com>
2013-05-22 02:44:35 +0400

    new func

--- jquery.scrollpage.js
+++ jquery.scrollpage.js
@@ -19,81 +19,101 @@
 *
 * <script>
 *     $("a.nav").scrollPage();
-* </scropt>
+* </script>
 * */
-(function ($) {
-    function SmartScroll(stepnumbers, wait) {
-        this.stepnumbers = stepnumbers;
-        this.step = 0;
-        this.wait = wait;
-        this.timeout_handler = null;
-        this.start = 0;
-        this.end = 0;
-        this.pos = 0;
-        this.speed = 2;
-    }
-    SmartScroll.prototype = {
-        log:function(txt){
-            //console.log( "SmartScroll." + txt);
-        },
-        dump:function(name){
-            //this.log( name + " pos=" + this.pos + " start="+this.start+" ,end="+this.end+" ,step="+this.step );
-        },
-        run:function (from, to) {
-            this.log("run() from="+from + " to="+to);
-            this.start = from;
-            this.end = to;
-            this.pos = from;
-            this.step = (to - from) / this.stepnumbers;
-            if (Math.abs(this.step) < 50) {
-                this.step = 50 * Math.abs(this.step) / this.step
-            }
-            if (this.timeout_handler != null) {
-                clearTimeout(this.timeout_handler);
-            }
-            this.dump("run()->scroll()");
-            this.scroll();
-        },
-        isStop: function(){
-            var stop = (this.pos-this.start)/(this.end-this.start);
-            return stop >= 1;
-        },
-        scroll:function () {
-            var s = this.step;
-            this.step *= this.speed;
-            this.pos += s;
-            if (this.isStop()) {
-                window.scrollTo($(window).scrollTop(),this.end);
-                this.clearTimeout();
-                this.dump("scroll()->stop");
-                this.log("s="+s);
-            } else {
-                this.dump("scroll()");
-                window.scrollBy(0, s);
-                this.setTimeout();
-            }
+(function ($) {       
+	var Scroller = function(){
+		this.timeout = null;		
+	}
+	Scroller.prototype = {
+		scroll:function(from, to){
+			var wHeight = $(window).height();
+			var pathHeight = to - from;
+			var move = from < to ? 'down' : 'up';
+			var delay = 80;
+			var memo = {
+				move: move,
+				delay: delay,
+				delayStep: 8,
+				delayCounter:0,
+				delayCounterStop:1,
+				pos: from,
+				from: from,				
+				to: to,
+				step: 5,
+				pathStop: wHeight/4
+			}
+			var _delay = function(memo){
+				if(memo.delayCounter >= memo.delayCounterStop){
+					memo.delay -= memo.delayStep;
+					memo.delayCounter = 0;	
+				} else {
+					memo.delayCounter += 1
+				}
+				
+				if(memo.delay < 0){
+					memo.delay = 0
+				}
+				var delta = memo.to - memo.pos;
+				if(memo.move == 'up'){ delta *= -1;}				
+				if(delta < memo.pathStop){
+					memo.delay = delay;					
+					memo.delayCounterStop = 0;					
+					memo.pathStop = 0;
+					return delay;
+				}
+				return memo.delay;
+			};
 
-        },
-        setTimeout:function () {
-            this.clearTimeout();
-            var self = this;
-            this.timeout_handler = setTimeout(function () {
-                self.scroll();
-            }, this.wait);
-        },
-        clearTimeout:function () {
-            this.dump("clearTimeout()");
-            if (this.timeout_handler != null) {
-                clearTimeout(this.timeout_handler);
-                this.timeout_handler = null;
-            }
-        }
-    };
-    var smart_scroll = new SmartScroll(10,100);
+			this.delay(_delay, memo, function(memo){
+				var pos = 0, stop = false;
+				if( memo.move == 'down'){
+					pos = memo.pos + memo.step;
+					stop = pos < memo.to
+				} else {
+					pos = memo.pos - memo.step;
+					stop = pos > memo.to
+				}					
+				if(stop){
+					window.scrollTo(0, pos);
+					memo.pos = pos;
+					return memo;	
+				} else {
+					window.scrollTo(0, memo.to);
+					return null;
+				}
+				
+			});						
+		},
+		delay: function(delay, memo, func){
+			if(this.timeout != null){
+				clearTimeout(this.timeout);
+				this.timeout = null;
+			}
+			var self = this;
+			var _delay = delay;
+			if(typeof(delay) == typeof(Function)){
+				_delay = delay(memo);
+			}			
+			this.timeout = setTimeout(function(){
+				if(memo == null) return;
+				var _memo = func(memo);
+				if(_memo!=null){
+					self.delay(delay, _memo, func);
+				}				
+			}, _delay);
+		}
+	}
     var scroll_to_aselector = function(s){
-        var a = $("a[name="+s+"]");
-        var top = a.position().top;
-        smart_scroll.run($(window).scrollTop(), top);
+    	var scroller = new Scroller()
+        var a = $("a[name="+s+"]");        
+        var from = $(window).scrollTop();
+        var to = a.position().top;
+        
+    	scroller.scroll(from, to);
+
+		//scrollBy(0,s)
+		//scrotllTo(from, to)
     };
     $.fn.scrollPage = function () {
         $(this).each(function(){
@@ -105,4 +125,3 @@
         });
     }
 })(jQuery);
-
